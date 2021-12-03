6219afbc6fee9f337df689f922d0ee47ebf57d22
Ozsbourn <mibaranin@gmail.com>
2021-12-03 12:43:04 +0500

    edits

--- ДЗ_1/ajax.js
+++ ДЗ_1/ajax.js
@@ -1,17 +1,17 @@
-function updateForm(){
+document.addEventListener("DOMContentLoaded", function(event){
 	response = fetch('./brands.dat').then(function(response) {
 		return response.text();
 	}).then((data) => {
 		let tmp = data.split('\n');
 
+		let doc, i;
 		let addDiv = document.getElementById("br_add");
-		for (let i = 0; i < tmp.length; i++) {
-			if (document.getElementById("br_val_" + i) != null) {
-				document.getElementById("br_val_" + i).innerHTML = tmp[i];
+		for (i = 0; i < tmp.length; i++) {
+			if ((doc = document.getElementById("br_val_" + i)) != null) {
+				doc.innerHTML = tmp[i];
 			} else {
-				addDiv.insertAdjacentHTML('afterend',
-					'<option id="br_val_' + i + '">' + tmp[i] + '</option>');
+				addDiv.insertAdjacentHTML('afterBegin', '<option id="br_val_added">' + tmp[i] + '</option>');
 			}
 		}
 	})
-}
+});
